0cfcb1e3993ff441b14adae727223c6d4856b634
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-10-14 12:34:09 +0200

    Ajout des PV max

--- eclipse/src/main/java/personnages/Unites.java
+++ eclipse/src/main/java/personnages/Unites.java
@@ -8,15 +8,17 @@ public class Unites {
 	private int magicalBaseDamage;
 	protected int range;
 	protected int actionPoint;
+	private int pvMax;
 	
 	public Unites(int pv, String name, int physicalBaseDamage, int magicalBaseDamage, int range, int actionPoint) {
 		super();
-		this.setPv(pv);
+		this.pv = pv;
 		this.name = name;
 		this.physicalBaseDamage = physicalBaseDamage;
 		this.setMagicalBaseDamage(magicalBaseDamage);
 		this.range = range;
 		this.actionPoint = actionPoint;
+		this.setPvMax(pv);
 	}
 
 	public int getMagicalBaseDamage() {
@@ -37,6 +39,17 @@ public class Unites {
 
 	public void setPv(int pv) {
 		this.pv += pv;
+		if (this.pv > this.pvMax) {
+			this.pv = this.pvMax;
+		}
+	}
+
+	public int getPvMax() {
+		return pvMax;
+	}
+
+	public void setPvMax(int pvMax) {
+		this.pvMax = pvMax;
 	}
 	
 	
