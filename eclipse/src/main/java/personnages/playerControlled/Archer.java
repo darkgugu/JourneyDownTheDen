fcac564f35f1f6dee7b1d8b65cec2bb82bffff30
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-10-20 15:09:38 +0200

    Ajout classe Archer et Warrior
    Attention : Classes copies de Magician, seul le baseName est changé

---
+++ eclipse/src/main/java/personnages/playerControlled/Archer.java
@@ -0,0 +1,31 @@
+package personnages.playerControlled;
+
+import capacites.Capacites;
+
+public class Archer extends Personnages{
+
+	private Capacites skills[] = new Capacites[10];
+	
+	public Capacites[] getSkills() {
+		return skills;
+	}
+
+
+	public void setSkills(Capacites a, Capacites b) {
+		this.skills[0] = a;
+		this.skills[1] = b;
+	}
+
+	public Archer() {
+		super(100, "Archer", 10, 50, 20, 5);
+
+	}
+
+	@Override
+	public String toString() {
+		
+		String spellBar = "Sort 1 : " + skills[0].getClass().getSimpleName() + "\nSort 2  : " + skills[1].getClass().getSimpleName();
+		return spellBar;
+	}
+
+}
