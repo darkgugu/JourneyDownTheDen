ed745b0b8b4062d2b8bf0d49fe4f9f3885f611f9
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-10-20 15:10:33 +0200

    Ajout de team[]

--- eclipse/src/main/java/jeu/Jeu.java
+++ eclipse/src/main/java/jeu/Jeu.java
@@ -5,32 +5,38 @@ import capacites.Soin;
 import personnages.IAControlled.Gobelin;
 import personnages.playerControlled.Healer;
 import personnages.playerControlled.Magician;
+import personnages.playerControlled.Warrior;
 
-public class Jeu {
+public class Jeu{
+	
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		Healer healer = new Healer();
 		Magician magician = new Magician();
 		Gobelin gobelin = new Gobelin();
-		
+		Warrior warrior = new Warrior();
+	
 		BouleDeFeu bdf = new BouleDeFeu();
 		Soin soin = new Soin();
 		
-		//healer.setSkills(bdf, soin);
+		Pregame team = new Pregame();
+		
+		team.setTeam(magician);
+		team.setTeam(healer);
+		team.setTeam(warrior);
+				
+		healer.setSkills(bdf, soin);
 		
 		System.out.println("Gobelin HP : " + gobelin.getPv() + "/" + gobelin.getPvMax());
 		
 		new BouleDeFeu().cast(magician, gobelin);
 		new BouleDeFeu().cast(magician, gobelin);
 
-		//soin.cast(healer, magician);
+		soin.cast(healer, magician);
 
 		System.out.println("Gobelin HP : " + gobelin.getPv() + "/" + gobelin.getPvMax());
 		
-		//System.out.println(healer);
-		
+		System.out.println(healer);
 	}
-
 }
