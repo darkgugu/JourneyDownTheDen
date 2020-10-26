d06230d1f5c78aa6b8a0d549366aa46b850d6ec1
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-10-26 17:28:41 +0100

    Ajout classe Deplacement
    Classe Deplacement ->
    méthode calculateCross et calculateDiag, prends en paramètres une position et un nombre de points de déplacements et renvoie une list des positions de mouvement possible

---
+++ eclipse/src/main/java/jeu/Deplacement.java
@@ -0,0 +1,47 @@
+package jeu;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deplacement {
+	
+	public List<SimpleEntry<Integer, Integer>> list = new ArrayList<SimpleEntry<Integer, Integer>>();
+	
+	public void calculateCross(int pm, int posx, int posy) {
+				
+		for (int i = 1; i <= pm; i++) {
+			
+			
+			add(posx - i, posy);
+			add(posx, posy + i);
+			add(posx + i, posy);
+			add(posx, posy - i);
+		}
+	
+	}
+	
+	public void calculateDiag(int pm, int posx, int posy) {
+		
+		if(pm == 0) {
+			
+			return;
+		}
+		for (int j = 1; j <= pm - 1; j++) {
+		
+			add(posx - pm + j, posy - j);
+			add(posx + pm - j, posy + j);
+			add(posx + j, posy - pm + j);
+			add(posx - j, posy + pm - j);
+		}
+		
+		calculateDiag(pm - 1, posx, posy);
+		
+	}
+
+	public void add(int x, int y) {
+		
+		list.add(new SimpleEntry<Integer, Integer>(x, y));
+		
+	}
+}
