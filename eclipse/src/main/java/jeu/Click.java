7ea4c6a7d9d6a304e4caa88c5cd4993b2f1d18c3
Marc <64917487+Kakuno77@users.noreply.github.com>
2020-11-02 11:05:39 +0100

    casesAround

---
+++ eclipse/src/main/java/jeu/Click.java
@@ -0,0 +1,20 @@
+package jeu;
+
+public class Click {
+
+	public int[] cases(int x, int y) {
+
+		int tab[] = new int[4];
+		int xcase = x / 60;
+		int ycase = y / 60;
+		int xpix = xcase * 60 + 30;
+		int ypix = ycase * 60 + 30;
+
+		tab[0] = xcase;
+		tab[1] = ycase;
+		tab[2] = xpix;
+		tab[3] = ypix;
+
+		return tab;
+	}
+}
