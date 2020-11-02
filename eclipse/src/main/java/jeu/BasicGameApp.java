3308eb378ad4b8e41a34e22f9e93386c700bc5b8
Marc <64917487+Kakuno77@users.noreply.github.com>
2020-11-02 15:30:51 +0100

    ab

--- eclipse/src/main/java/jeu/BasicGameApp.java
+++ eclipse/src/main/java/jeu/BasicGameApp.java
@@ -23,7 +23,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
-import fxglExemple.Deplacement;
+import jeu.Deplacement;
 
 public class BasicGameApp extends GameApplication {
 	private Entity player;
