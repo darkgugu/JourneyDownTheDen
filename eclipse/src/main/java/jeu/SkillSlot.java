551052df7d1a33dcf5a40a00bf7085c34bcef722
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-12-18 16:06:37 +0100

    Update SkillSlot.java

--- eclipse/src/main/java/jeu/SkillSlot.java
+++ eclipse/src/main/java/jeu/SkillSlot.java
@@ -7,17 +7,10 @@ public abstract class SkillSlot {
 	public static int isSkillSlot(int x, int y) {
 		
 		ArrayList<Integer> slotsCoord = new ArrayList<Integer>(10);
-		//Collections.addAll(slotsCoord, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21);
-		slotsCoord.add(12);
-		slotsCoord.add(13);
-		slotsCoord.add(14);
-		slotsCoord.add(15);
-		slotsCoord.add(16);
-		slotsCoord.add(17);
-		slotsCoord.add(18);
-		slotsCoord.add(19);
-		slotsCoord.add(20);
-		slotsCoord.add(21);
+		for (int i = 12;i <= 21; i++) {
+			slotsCoord.add(i);
+		}
+		
 		int[] tab = Click.cases(x, y);
 
 		
