a6f66a7afa337eee673a648ec6302555da5345e7
Marc <64917487+Kakuno77@users.noreply.github.com>
2021-01-11 10:29:34 +0100

    Cases bloquées

--- eclipse/src/main/java/jeu/Click.java
+++ eclipse/src/main/java/jeu/Click.java
@@ -1,10 +1,12 @@
 package jeu;
 
+import java.util.Hashtable;
+
 public class Click {
-  
+
 	public static int[] cases(int x, int y) {
 
-		int tab[] = new int[4];
+		int tab[] = new int[6];
 		int xcase = x / 60;
 		int ycase = y / 60;
 		int xpix = xcase * 60;
@@ -14,7 +16,19 @@ public class Click {
 		tab[1] = ycase;
 		tab[2] = xpix;
 		tab[3] = ypix;
-
+		tab[4] = 60;
+		tab[5] = 120;
 		return tab;
 	}
+
+	// Table de hachage contenant les cases bloquées
+	Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
+
+	public static int[] casesNonValides(int x, int y) {
+		int tabNonValid[] = new int[50];
+		tabNonValid[0] = 60;
+		tabNonValid[1] = 60;
+		return tabNonValid;
+	}
+
 }
