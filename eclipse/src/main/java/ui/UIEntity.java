fda30a9eb07457b5da84f3ba615894bb6631c471
darkgugu <duret@et.intechinfo.fr>
2021-01-19 11:07:18 +0100

    Update UIEntity.java

--- eclipse/src/main/java/ui/UIEntity.java
+++ eclipse/src/main/java/ui/UIEntity.java
@@ -5,8 +5,6 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
-import com.almasb.fxgl.entity.components.CollidableComponent;
-
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import jeu.EntityType;
