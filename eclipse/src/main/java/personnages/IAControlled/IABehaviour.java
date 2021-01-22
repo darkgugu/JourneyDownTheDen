677ab20c95342d768da12bd513038e75c8949935
darkgugu <duret@et.intechinfo.fr>
2021-01-22 02:51:17 +0100

    IA Déplacement
    Gestion du déplacement en croix positif +x et +y si
    Xgobelin == Xcible ||
    Ygobelin == Ycible

--- eclipse/src/main/java/personnages/IAControlled/IABehaviour.java
+++ eclipse/src/main/java/personnages/IAControlled/IABehaviour.java
@@ -61,8 +61,8 @@ public class IABehaviour {
 		
 		Player closePlayer = players[0];
 		double closeDist = dist[0];
-		double xdir;
-		double ydir;
+		double xtar;
+		double ytar;
 		
 		if(dist[0] <= dist[1] && dist[0] <= dist[2]) {
 			closePlayer = players[0];
@@ -84,10 +84,32 @@ public class IABehaviour {
 		if(closeDist <= unit.getType().getAggroRange() * 60 && closeDist > 60) {
 			
 			GameLog.setGameLog(closePlayer.getName() + " est à portée de " + unit.getName() + " et n'est pas au cac");
-			if(unit.getPosition().getX() < closePlayer.getPosition().getX()) {
+			if(unit.getPosition().getX() == closePlayer.getPosition().getX()) {
 				
+				xtar = unit.getPosition().getX();
+				double delta = closePlayer.getPosition().getY() - unit.getPosition().getY();
+				ytar = unit.getPosition().getY() + tar2(Math.abs(delta), unit.getType().getMovePoint());
+				GameLog.setGameLog("Deplacement en case " + xtar/60 + "," + ytar/60);
 				
 			}
+			if(unit.getPosition().getY() == closePlayer.getPosition().getY()) {
+				
+				ytar = unit.getPosition().getY();
+				double delta = closePlayer.getPosition().getX() - unit.getPosition().getX();
+				xtar = unit.getPosition().getX() + tar2(Math.abs(delta), unit.getType().getMovePoint());
+				GameLog.setGameLog("Deplacement en case " + xtar/60 + "," + ytar/60);
+			}
+			
+		}
+	}
+	
+	public static double tar2(double delta, int pm) {
+		
+		if(delta > pm * 60) {
+			return pm * 60; 
+		}
+		else {
+			return tar2(delta, pm - 1);
 		}
 	}
 	
