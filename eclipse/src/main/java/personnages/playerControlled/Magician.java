62543789d5acd62889300b8ba94b3fcddc20c885
darkgugu <duret@et.intechinfo.fr>
2021-01-24 02:36:34 +0100

    Update de skills[]

--- eclipse/src/main/java/personnages/playerControlled/Magician.java
+++ eclipse/src/main/java/personnages/playerControlled/Magician.java
@@ -1,14 +1,14 @@
 package personnages.playerControlled;
 
 import capacites.Fireball;
-import capacites.Soin;
+import capacites.MagicalDamageBuff;
 
 public class Magician extends Personnages{
 
 
 	public Magician() {
-		super(100, "Magician", 10, 50, 20, 5, 1);
-		skills[0] = new Soin();
+		super(100, "Magician", 1, 5, 20, 5, 1);
+		skills[0] = new MagicalDamageBuff();
 		skills[1] = new Fireball();
 
 	}
