30e62da6f4f74e8e72879562186579a323bae327
darkgugu <duret@et.intechinfo.fr>
2021-01-24 19:40:05 +0100

    Kill Unit updates

--- eclipse/src/main/java/jeu/KillUnit.java
+++ eclipse/src/main/java/jeu/KillUnit.java
@@ -7,6 +7,7 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.GameWorld;
 
 import javafx.geometry.Point2D;
+import ui.GameLog;
 
 public class KillUnit {
 	public KillUnit(GameWorld gameWorld, Player playerRed, Player playerBlue, Player playerGreen) {
@@ -15,30 +16,32 @@ public class KillUnit {
 	public void checkKill(GameWorld gameWorld, Player playerRed, Player playerBlue, Player playerGreen,
 			Player playerSelected) {
 		
-		
 
-		if (playerRed.getHeroClass().getPv() <= 0) {
+		if (playerRed.getHeroClass().getPv() <= 0 && playerRed.getHeroClass().isDead() == false) {
 			List<Entity> target = FXGL.getApp().getGameWorld()
 					.getEntitiesAt(new Point2D(playerRed.getPosition().getX(), playerRed.getPosition().getY()));
 			for (Entity entity : target) {
 				entity.removeFromWorld();
 			}
+			playerRed.getHeroClass().setDead(true);
 			
 		}
-		if (playerBlue.getHeroClass().getPv() <= 0) {
+		if (playerBlue.getHeroClass().getPv() <= 0 && playerBlue.getHeroClass().isDead() == false) {
 			List<Entity> target = FXGL.getApp().getGameWorld()
 					.getEntitiesAt(new Point2D(playerBlue.getPosition().getX(), playerBlue.getPosition().getY()));
 			for (Entity entity : target) {
 				entity.removeFromWorld();
 			}
+			playerBlue.getHeroClass().setDead(true);
 			
 		}
-		if (playerGreen.getHeroClass().getPv() <= 0) {
+		if (playerGreen.getHeroClass().getPv() <= 0 && playerGreen.getHeroClass().isDead() == false) {
 			List<Entity> target = FXGL.getApp().getGameWorld()
 					.getEntitiesAt(new Point2D(playerGreen.getPosition().getX(), playerGreen.getPosition().getY()));
 			for (Entity entity : target) {
 				entity.removeFromWorld();
 			}
+			playerGreen.getHeroClass().setDead(true);
 			
 		}
 
